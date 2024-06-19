318075d431fb07e64150166cc774b789140fae4f
mohamed abd elkhalk <01147018091.m@gmial.com>
2024-06-19 22:07:35 +0300

    add sidenav

---
+++ client/src/components/shared/DashboardBar.tsx
@@ -0,0 +1,33 @@
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link } from "react-router-dom";
+const DashboardBar = ({ tab }: { tab: string }) => {
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            lable={"User"}
+            labelColor="dark"
+            className="cursor-pointer"
+          >
+            Profile
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashboardBar;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashboardBar from "../components/shared/DashboardBar";
+import DashboardProfile from "../components/shared/DashboardProfile";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState<string>("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabParma = urlParams.get("tab");
+    if (tabParma) {
+      setTab(tabParma);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* sideNav */}
+        <DashboardBar tab={tab} />
+      </div>
+      {/* profile */}
+      {tab === "profile" ? <DashboardProfile /> : ""}
+    </div>
+  );
 };
 
 export default Dashboard;
